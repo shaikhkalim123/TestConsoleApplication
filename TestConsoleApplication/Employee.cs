fcd1ae6b4e6f1092cd2ba16f1efe62d5812cae5e
shaikhkalim123 <shaikhkalim123@ymail.com>
2018-07-19 13:13:20 +0530

    adding employee class

---
+++ TestConsoleApplication/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApplication
+{
+    class Employee
+    {
+        private int empCode;
+
+        public int EmpCode
+        {
+            get { return empCode; }
+            set { empCode = value; }
+        }
+
+
+        private string empName;
+
+        public string EmpName
+        {
+            get { return empName; }
+            set { empName = value; }
+        }
+
+    }
+}
